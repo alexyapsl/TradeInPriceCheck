203e555ae3055ef2f090c6d587620de8091642b8
alexyapsl <alexyapsl@gmail.com>
2026-06-11 14:25:39 +0800

    feat: gift selector pricing + narrow-screen subtitle
    - Gift modal now shows 'Add ' (blue) for paid gifts and 'Free gift' (green) for free items (from Column E of gift spreadsheet)
    - Updated giftMap with real prices
    - Rebuild script now supports Gift1Price/Gift2Price/Gift3Price columns for future CSV imports
    - 'Hong Kong / Macau only' subtitle already present and responsive on narrow screens (375px)
    - Step 1-5 previously implemented; only 6-7 remained
    
    Refs: gift sheet pricing, responsive step circles, header styling parity

---
+++ rebuild.ps1
@@ -0,0 +1,93 @@
+﻿const csv = `Model Name,Storage,Color en_HK,color zh_HK,Estore Exclusive,SKU,oosLink hk,oosLink hk_em,Gift1,Gift2,Gift3,Gift1Price,Gift2Price,Gift3Price
+Galaxy S26 Ultra,1TB+16GB,Black,星曜黑,N,SM-S9480ZKPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,1TB+16GB,Sky Blue,星澄藍,N,SM-S9480LBPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,1TB+16GB,Cobalt Violet,星暮紫,N,SM-S9480ZVPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,1TB+16GB,Pink Gold,星瑰金,Y,SM-S9480ZDPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,1TB+16GB,Silver Shadow,星月銀,Y,SM-S9480ZSPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,Black,星曜黑,N,SM-S9480ZKOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,Sky Blue,星澄藍,N,SM-S9480LBOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,Cobalt Violet,星暮紫,N,SM-S9480ZVOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,White,星霜白,N,SM-S9480ZWOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,Pink Gold,星瑰金,Y,SM-S9480ZDOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,512GB+12GB,Silver Shadow,星月銀,Y,SM-S9480ZSOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,Black,星曜黑,N,SM-S9480ZKITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,Sky Blue,星澄藍,N,SM-S9480LBITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,Cobalt Violet,星暮紫,N,SM-S9480ZVITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,White,星霜白,N,SM-S9480ZWITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,Pink Gold,星瑰金,Y,SM-S9480ZDITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26 Ultra,256GB+12GB,Silver Shadow,星月銀,N,SM-S9480ZSITGY,https://www.samsung.com/hk/smartphones/galaxy-s26-ultra/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26-ultra/buy/,S26U_Gift001,S26U_Gift002,S26U_Gift003
+Galaxy S26+,512GB+12GB,Black,星曜黑,N,SM-S9470ZKOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,512GB+12GB,Sky Blue,星澄藍,N,SM-S9470LBOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,512GB+12GB,Cobalt Violet,星暮紫,N,SM-S9470ZVOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,512GB+12GB,White,星霜白,N,SM-S9470ZWOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,512GB+12GB,Pink Gold,星瑰金,Y,SM-S9470ZDOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,256GB+12GB,Silver Shadow,星月銀,Y,SM-S9470ZSOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,256GB+12GB,Black,星曜黑,N,SM-S9470ZKITGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26+,256GB+12GB,Sky Blue,星澄藍,N,SM-S9470LBITGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26P_Gift001,S26P_Gift002,S26P_Gift003
+Galaxy S26,512GB+12GB,Black,星曜黑,N,SM-S9420ZKPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,512GB+12GB,Sky Blue,星澄藍,N,SM-S9420LBPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,512GB+12GB,Cobalt Violet,星暮紫,N,SM-S9420ZVPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,512GB+12GB,White,星霜白,N,SM-S9420ZWPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,512GB+12GB,Pink Gold,星瑰金,Y,SM-S9420ZDPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,512GB+12GB,Silver Shadow,星月銀,Y,SM-S9420ZSPTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy S26,256GB+12GB,Black,星曜黑,N,SM-S9420ZKOTGY,https://www.samsung.com/hk/smartphones/galaxy-s26/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-s26/buy/,S26B_Gift001,S26B_Gift002,S26B_Gift003
+Galaxy Z Fold7,1TB+16GB,Blue Shadow,星塵藍,N,SM-F9660DBHTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,1TB+16GB,Jetblack,曜石黑,N,SM-F9660ZKHTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,512GB+12GB,Blue Shadow,星塵藍,N,SM-F9660DBGTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,512GB+12GB,Silver Shadow,霧銀灰,N,SM-F9660ZSGTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,512GB+12GB,Jetblack,曜石黑,N,SM-F9660ZKGTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,512GB+12GB,Mint,薄荷綠,Y,SM-F9660LGGTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,256GB+12GB,Blue Shadow,星塵藍,N,SM-F9660DBDTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,256GB+12GB,Silver Shadow,霧銀灰,N,SM-F9660ZSDTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,256GB+12GB,Jetblack,曜石黑,N,SM-F9660ZKDTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Fold7,256GB+12GB,Mint,薄荷綠,Y,SM-F9660LGDTGY,https://www.samsung.com/hk/smartphones/galaxy-z-fold7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-fold7/buy/,Fold7_Gift001,Fold7_Gift002,Fold7_Gift003
+Galaxy Z Flip7,512GB+12GB,Blue Shadow,星塵藍,N,SM-F7660DBETGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,512GB+12GB,Jetblack,曜石黑,N,SM-F7660ZKETGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,512GB+12GB,Coralred,珊瑚紅,N,SM-F7660ZRETGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,512GB+12GB,Mint,薄荷綠,Y,SM-F7660LGETGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,256GB+12GB,Blue Shadow,星塵藍,N,SM-F7660DBATGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,256GB+12GB,Jetblack,曜石黑,N,SM-F7660ZKATGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,256GB+12GB,Coralred,珊瑚紅,N,SM-F7660ZRATGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003
+Galaxy Z Flip7,256GB+12GB,Mint,薄荷綠,Y,SM-F7660LGATGY,https://www.samsung.com/hk/smartphones/galaxy-z-flip7/buy/,https://www.samsung.com/hk_en/smartphones/galaxy-z-flip7/buy/,Flip7_Gift001,Flip7_Gift002,Flip7_Gift003`;
+
+const lines = csv.trim().split('\n').slice(1); // skip header
+const byProduct = {};
+
+lines.forEach(line => {
+  const cols = line.split(',');
+  const model = cols[0];
+  const storage = cols[1];
+  const color_en = cols[2];
+  const estore = cols[4] === 'Y';
+  const sku = cols[5];
+  const gift1 = cols[8];
+  const gift2 = cols[9];
+  const gift3 = cols[10];
+  const gift1Price = parseInt(cols[11]) || 0;
+  const gift2Price = parseInt(cols[12]) || 0;
+  const gift3Price = parseInt(cols[13]) || 0;
+
+  if (!byProduct[model]) byProduct[model] = {};
+  if (!byProduct[model][storage]) byProduct[model][storage] = { colors: [] };
+
+  byProduct[model][storage].colors.push({
+    name: color_en,
+    sku: sku,
+    gift1, gift2, gift3,
+    gift1Price, gift2Price, gift3Price
+  });
+});
+
+// Build final structure
+const result = Object.entries(byProduct).map(([name, storages]) => {
+  const storageOptions = Object.entries(storages).map(([storage, data]) => ({
+    storage,
+    price: 0, // price not in this sheet
+    colors: data.colors
+  }));
+  return { name, storageOptions };
+});
+
+fs.writeFileSync('recommended-products.json', JSON.stringify(result, null, 2));
+console.log('Rebuilt recommended-products.json with', result.length, 'products');
+console.log(result.map(r => r.name));
